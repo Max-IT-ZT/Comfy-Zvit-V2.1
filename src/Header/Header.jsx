d06cfd910c195f1cb84b6852b4f5bcfa5b3dd7cf
Max-IT-ZT <babuskomax@gmail.com>
2024-09-06 01:31:24 +0300

    burger done

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,24 +1,62 @@
+import { useState } from "react";
 import css from "./Header.module.css";
+import { FaCode } from "react-icons/fa";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={css.header}>
-      <a href="" className={css.logoLink}>
-        <img
-          src="../../public/logo3.png"
-          alt="MaxCrafter"
-          className={css.logoImg}
-        />
+      <a href="#" className={css.logoLink}>
+        <div className={css.logo}>
+          <p className={css.logoText}>
+            Max<span className={css.logoTextSpan}>Crafter</span>
+            <FaCode />
+          </p>
+        </div>
       </a>
       <nav className={css.menu}>
-        <ul className={css.menuList}>
-          <li className={css.menuItem}>План</li>
-          <li className={css.menuItem}>Звіт</li>
-          <li className={css.menuItem}>Галерея</li>
-          <li className={css.menuItem}>Контакти</li>
-          <li className={css.menuItem}>Гаманець</li>
-          <li className={css.menuItem}>Графік</li>
+        <ul
+          className={`${css.menuList} ${
+            menuOpen
+              ? `${css.menuListOpen} ${css.menuVisible}`
+              : css.menuListOpen
+          }`}
+        >
+          <li className={css.menuItem}>
+            <button className={css.menuBtn}>План</button>
+          </li>
+          <li className={css.menuItem}>
+            <button className={css.menuBtn}>Звіт</button>
+          </li>
+          <li className={css.menuItem}>
+            <button className={css.menuBtn}>Галерея</button>
+          </li>
+          <li className={css.menuItem}>
+            <button className={css.menuBtn}>Контакти</button>
+          </li>
+          <li className={css.menuItem}>
+            <button className={css.menuBtn}>Гаманець</button>
+          </li>
+          <li className={css.menuItem}>
+            <button className={css.menuBtn}>Графік</button>
+          </li>
         </ul>
+        <div className={css.burgerMenu} onClick={toggleMenu}>
+          <span
+            className={`${css.burgerLine} ${menuOpen ? css.line1 : ""}`}
+          ></span>
+          <span
+            className={`${css.burgerLine} ${menuOpen ? css.line2 : ""}`}
+          ></span>
+          <span
+            className={`${css.burgerLine} ${menuOpen ? css.line3 : ""}`}
+          ></span>
+        </div>
       </nav>
     </header>
   );
